4fa8582e500372421e566ab4bd16fbb6dbe7b1f1
sozooo <110661290+sozooo@users.noreply.github.com>
2024-07-23 14:01:24 +0300

    Initial commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Renderer))]
+public class Cube : MonoBehaviour
+{
+    private Platform _collidedPlataform;
+    private Renderer _renderer;
+
+    public Renderer Renderer => _renderer;
+
+    public event Action<Cube> Collided;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.TryGetComponent(out Platform platform))
+        {
+            if(_collidedPlataform == null)
+            {
+                _collidedPlataform = platform;
+
+                SetRandomColor();
+                Collided?.Invoke(this);
+            }
+        }
+    }
+
+    public void SetRandomColor()
+    {
+        _renderer.material.color = Random.ColorHSV();
+    }
+}
---
+++ Assets/Scripts/CubePool.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CubePool : MonoBehaviour
+{
+    [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private float _minTimeToPool = 2;
+    [SerializeField] private float _maxTimeToPool = 5;
+
+    private Queue<Cube> _cubes;
+
+    private void Awake()
+    {
+        _cubes = new Queue<Cube>();
+    }
+
+    public void Add(Cube cube)
+    {
+        cube.Collided -= StartTimer;
+
+        cube.gameObject.SetActive(false);
+        _cubes.Enqueue(cube);
+    }
+
+    public Cube Take()
+    {
+        Cube cube;
+
+        if(_cubes.Count == 0)
+            cube = Instantiate(_cubePrefab);
+        else
+            cube = _cubes.Dequeue();
+
+        cube.Collided += StartTimer;
+
+        return cube;
+    }
+
+    public void Reset()
+    {
+        _cubes.Clear();
+    }
+
+    private void StartTimer(Cube cube)
+    {
+        float _timeToPool = Random.Range(_minTimeToPool, _maxTimeToPool + 1);
+
+        StartCoroutine(Timer(cube, _timeToPool));
+    }
+
+    private IEnumerator Timer(Cube cube, float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Add(cube);
+    }
+}
